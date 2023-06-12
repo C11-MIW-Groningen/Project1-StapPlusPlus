c94d52cc811aaba86b2aad1188091e48f55ce9d0
Thijs <thijs05@hotmail.com>
2023-06-12 13:47:49 +0200

    initial commit

---
+++ src/test/java/nl/miwgroningen/ch11/stap/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.ch11.stap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
