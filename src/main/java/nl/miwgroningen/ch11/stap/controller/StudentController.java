f6fd4c663f31f3649c4bf82678f0a077c8622028
Tristan Meinsma <tristan.meinsma@gmail.com>
2023-06-12 15:25:07 +0200

    student & subject controller + overview page

--- src/main/java/nl/miwgroningen/ch11/stap/controller/StudentController.java
+++ src/main/java/nl/miwgroningen/ch11/stap/controller/StudentController.java
@@ -1,11 +1,22 @@
 package nl.miwgroningen.ch11.stap.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
- * Author: Thijs Harleman
- * Created at 14:01 on 12 Jun 2023
- * Purpose: Handle interactions with webpages about students
+ * @author Tristan Meinsma
+ * This handles interaction on the webpage about students
  */
+@Controller
+@RequestMapping("/student")
+@RequiredArgsConstructor
 
 public class StudentController {
 
+    @GetMapping("/all")
+    private String showStudentOverview() {
+        return "studentOverview";
+    }
 }
--- src/main/java/nl/miwgroningen/ch11/stap/controller/SubjectController.java
+++ src/main/java/nl/miwgroningen/ch11/stap/controller/SubjectController.java
@@ -1,12 +1,26 @@
 package nl.miwgroningen.ch11.stap.controller;
 
+import lombok.RequiredArgsConstructor;
+import nl.miwgroningen.ch11.stap.repositories.SubjectRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
- * Author: Thijs Harleman
- * Created at 14:02 on 12 Jun 2023
- * Purpose: Handle interactions with webpages about course subjects
+ * @author Tristan Meinsma
+ * This handles interaction with webpage about subjects
  */
 
-
+@Controller
+@RequestMapping("/subject")
+@RequiredArgsConstructor
 public class SubjectController {
+    private final SubjectRepository subjectRepository;
 
+    @GetMapping( "/all")
+    private String showSubjectOverview(Model model) {
+        return "subjectOverview";
+    }
 }
+
