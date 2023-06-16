c60d95b0ed87688a2b8a86ba9b03a103bd530a0f
Thijs <thijs05@hotmail.com>
2023-06-16 10:13:12 +0200

    Hide learning goals when not authenticated

--- src/main/java/nl/miwgroningen/ch11/stap/controller/SubjectController.java
+++ src/main/java/nl/miwgroningen/ch11/stap/controller/SubjectController.java
@@ -2,6 +2,7 @@ package nl.miwgroningen.ch11.stap.controller;
 
 import lombok.RequiredArgsConstructor;
 import nl.miwgroningen.ch11.stap.model.Subject;
+import nl.miwgroningen.ch11.stap.model.Teacher;
 import nl.miwgroningen.ch11.stap.repositories.LearningGoalRepository;
 import nl.miwgroningen.ch11.stap.repositories.SubjectRepository;
 import nl.miwgroningen.ch11.stap.repositories.TeacherRepository;
@@ -10,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -29,7 +32,7 @@ public class SubjectController {
     private String showSubjectForm(Model model) {
         model.addAttribute("subject", new Subject());
         model.addAttribute("allLearningGoals", learningGoalRepository.findAll());
-        model.addAttribute("allTeachers", teacherRepository.findAll());
+        model.addAttribute("allTeachers", getAllTeachersSorted());
 
         return "subjectForm";
     }
@@ -41,7 +44,7 @@ public class SubjectController {
         if (optionalSubject.isPresent()) {
             model.addAttribute("subject", optionalSubject.get());
             model.addAttribute("allLearningGoals", learningGoalRepository.findAll());
-            model.addAttribute("allTeachers", teacherRepository.findAll());
+            model.addAttribute("allTeachers", getAllTeachersSorted());
 
             return "subjectForm";
         }
@@ -65,6 +68,13 @@ public class SubjectController {
         optionalSubject.ifPresent(subjectRepository::delete);
 
         return "redirect:/";
+    }
+
+    private List<Teacher> getAllTeachersSorted() {
+        List<Teacher> allTeachers = teacherRepository.findAll();
+        Collections.sort(allTeachers);
+
+        return allTeachers;
     }
 }
 
