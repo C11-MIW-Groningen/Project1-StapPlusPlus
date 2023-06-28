bdaecbd9129f352a98295eaa7c04057c2f42ff30
Tristan Meinsma <tristan.meinsma@gmail.com>
2023-06-28 15:22:18 +0200

    even better subject test

--- src/test/java/nl/miwgroningen/ch11/stap/model/SubjectTest.java
+++ src/test/java/nl/miwgroningen/ch11/stap/model/SubjectTest.java
@@ -3,6 +3,7 @@ package nl.miwgroningen.ch11.stap.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,16 +14,25 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SubjectTest {
 
-    private Subject createSubjectWithDuration(int duration) {
-        Subject subject = new Subject();
+    private Subject subject;
+
+    private void createSubjectWithDuration(int duration) {
+        subject = new Subject();
         subject.setDuration(duration);
-        return subject;
+    }
+
+    @ParameterizedTest
+    @DisplayName("Should throw illegal argument exception")
+    @ValueSource(ints = {0, -1, -100})
+    void shouldThrowIllegalArgumentException(int invalidNumberOfDays) {
+        assertThrows(IllegalArgumentException.class,
+                () -> createSubjectWithDuration(invalidNumberOfDays));
     }
 
     @Test
     @DisplayName("Should return 1 day")
     void shouldReturn1Day() {
-        Subject subject = createSubjectWithDuration(1);
+        createSubjectWithDuration(1);
         String expectedString = "1 dag";
 
         assertEquals(expectedString, subject.getDurationString());
@@ -32,30 +42,18 @@ class SubjectTest {
     @DisplayName("Should show the correct amount of days")
     @ValueSource(ints = {2, 5, 9})
     void shouldShowTheCorrectAmountOfDays(int expectedNumberOfDays) {
-        Subject subject = createSubjectWithDuration(expectedNumberOfDays);
+        createSubjectWithDuration(expectedNumberOfDays);
         String expectedString = expectedNumberOfDays + " dagen";
 
         assertEquals(expectedString, subject.getDurationString());
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Should show correct amount of weeks")
-    void shouldShowCorrectAmountOfWeeks() {
-        Subject subject1 = createSubjectWithDuration(10);
-        String expectedString1 = "2 weken";
+    @CsvSource({"10, 2 weken", "12, 2 weken", "13, 3 weken"})
+    void shouldShowCorrectAmountOfWeeks(int duration, String expectedString) {
+        createSubjectWithDuration(duration);
 
-        Subject subject2 = createSubjectWithDuration(12);
-        String expectedString2 = "2 weken";
-
-        Subject subject3 = createSubjectWithDuration(13);
-        String expectedString3 = "3 weken";
-
-        Subject subject4 = createSubjectWithDuration(17);
-        String expectedString4 = "3 weken";
-
-        assertAll(() -> assertEquals(expectedString1, subject1.getDurationString()),
-                () -> assertEquals(expectedString2, subject2.getDurationString()),
-                () -> assertEquals(expectedString3, subject3.getDurationString()),
-                () -> assertEquals(expectedString4, subject4.getDurationString()));
+        assertEquals(expectedString, subject.getDurationString());
     }
 }
