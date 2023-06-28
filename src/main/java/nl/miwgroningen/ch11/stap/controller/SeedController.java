dca595a171f406d29ae6df2b90c43c27b5371d5f
Tristan Meinsma <tristan.meinsma@gmail.com>
2023-06-28 11:48:41 +0200

    removed dead import cachable

--- src/main/java/nl/miwgroningen/ch11/stap/controller/SeedController.java
+++ src/main/java/nl/miwgroningen/ch11/stap/controller/SeedController.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.Cacheable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
