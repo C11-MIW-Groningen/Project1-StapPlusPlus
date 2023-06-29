12ded1405544a64da64f21100c78838baf003c0f
Thijs <thijs05@hotmail.com>
2023-06-29 09:31:51 +0200

    cleanup pdfexporter

--- src/main/java/nl/miwgroningen/ch11/stap/pdf/PDFExporter.java
+++ src/main/java/nl/miwgroningen/ch11/stap/pdf/PDFExporter.java
@@ -20,6 +20,12 @@ import java.io.IOException;
 
 @RequiredArgsConstructor
 public class PDFExporter {
+    private static final float[] EXAM_INFO_COLUMN_WIDTHS = {3.0f, 9.0f};
+    private static final float[] QUESTIONS_COLUMN_WIDTHS = {1.5f, 1.5f, 9.0f};
+    private static final int TABLE_SPACING = 10;
+    private static final float TABLE_WIDTH_PERCENTAGE = 100f;
+    private static final int TITLE_FONT_SIZE = 18;
+
     private final StudentExam studentExam;
 
     public void export(HttpServletResponse response) throws DocumentException, IOException {
@@ -28,13 +34,13 @@ public class PDFExporter {
 
             document.open();
             Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-            font.setSize(18);
+            font.setSize(TITLE_FONT_SIZE);
 
             Paragraph title = new Paragraph(String.format("%s, %s",
                     studentExam.getExam().getDisplayExamTitle(), studentExam.getExam().getDisplayExamSubtitle()), font);
 
             document.add(title);
-            document.add(getTopTable());
+            document.add(getExamInfoTable());
             document.add(getExamQuestionsTable());
         } catch (DocumentException documentException) {
             System.err.println(documentException.getMessage());
@@ -52,7 +58,7 @@ public class PDFExporter {
         return stringBuilder.toString();
     }
 
-    private String[] getTopTableValues() {
+    private String[] getExamInfoTableValues() {
         return new String[] {
                 studentExam.getStudent().getDisplayName(),
                 studentExam.getStudent().getSchoolEmail(),
@@ -65,13 +71,19 @@ public class PDFExporter {
                 getLearningGoalsString()};
     }
 
+    private PdfPTable buildNewTable(float[] columnWidths) {
+        PdfPTable table = new PdfPTable(columnWidths.length);
+        table.setWidthPercentage(TABLE_WIDTH_PERCENTAGE);
+        table.setWidths(columnWidths);
+        table.setSpacingBefore(TABLE_SPACING);
+
+        return table;
+    }
+
     private PdfPTable getExamQuestionsTable() {
         String[] columnNames = {"Vraag", "Punten", "Feedback"};
 
-        PdfPTable table = new PdfPTable(3);
-        table.setWidthPercentage(100f);
-        table.setWidths(new float[] {1.5f, 1.5f, 9f});
-        table.setSpacingBefore(10);
+        PdfPTable table = buildNewTable(QUESTIONS_COLUMN_WIDTHS);
 
         for (String columnName : columnNames) {
             table.addCell(columnName);
@@ -87,15 +99,12 @@ public class PDFExporter {
         return table;
     }
 
-    private PdfPTable getTopTable() {
+    private PdfPTable getExamInfoTable() {
         String[] rowNames = {"Student:", "Email:", "Resultaat", "Punten behaald:", "Docent:", "Cohort:", "Vak:",
                 "Leerdoelen:"};
-        String[] rowValues = getTopTableValues();
+        String[] rowValues = getExamInfoTableValues();
 
-        PdfPTable table = new PdfPTable(2);
-        table.setWidthPercentage(100f);
-        table.setWidths(new float[] {2.0f, 10.0f});
-        table.setSpacingBefore(10);
+        PdfPTable table = buildNewTable(EXAM_INFO_COLUMN_WIDTHS);
 
         for (int row = 0; row < rowNames.length; row++) {
             table.addCell(rowNames[row]);
