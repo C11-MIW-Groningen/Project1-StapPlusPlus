96aaaf2f68fd8c1faf4a3a7067ce1bf14d19e9cf
Thijs <thijs05@hotmail.com>
2023-06-29 15:06:19 +0200

    remove table cell borders from pdfexporter

--- src/main/java/nl/miwgroningen/ch11/stap/pdf/PDFExporter.java
+++ src/main/java/nl/miwgroningen/ch11/stap/pdf/PDFExporter.java
@@ -2,6 +2,7 @@ package nl.miwgroningen.ch11.stap.pdf;
 
 import com.lowagie.text.*;
 import com.lowagie.text.Font;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import lombok.RequiredArgsConstructor;
@@ -28,19 +29,34 @@ public class PDFExporter {
 
     private final StudentExam studentExam;
 
+    private final PdfPCell cell = new PdfPCell();
+
+    private PdfPTable buildNewTable(float[] columnWidths) {
+        PdfPTable table = new PdfPTable(columnWidths.length);
+        table.setWidthPercentage(TABLE_WIDTH_PERCENTAGE);
+        table.setWidths(columnWidths);
+        table.setSpacingBefore(TABLE_SPACING);
+
+        return table;
+    }
+
     public void export(HttpServletResponse response) throws DocumentException, IOException {
         try (Document document = new Document(PageSize.A4)) {
             PdfWriter.getInstance(document, response.getOutputStream());
 
             document.open();
+
             Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
             font.setSize(TITLE_FONT_SIZE);
+
+            cell.setBorder(Rectangle.NO_BORDER);
 
             Paragraph title = new Paragraph(String.format("%s, %s",
                     studentExam.getExam().getDisplayExamTitle(), studentExam.getExam().getDisplayExamSubtitle()), font);
 
             document.add(title);
             document.add(getExamInfoTable());
+            document.add(getExamQuestionsHeaderTable());
             document.add(getExamQuestionsTable());
         } catch (DocumentException documentException) {
             System.err.println(documentException.getMessage());
@@ -71,29 +87,32 @@ public class PDFExporter {
                 getLearningGoalsString()};
     }
 
-    private PdfPTable buildNewTable(float[] columnWidths) {
-        PdfPTable table = new PdfPTable(columnWidths.length);
-        table.setWidthPercentage(TABLE_WIDTH_PERCENTAGE);
-        table.setWidths(columnWidths);
-        table.setSpacingBefore(TABLE_SPACING);
+    private PdfPTable getExamQuestionsHeaderTable() {
+        String[] columnNames = {"Vraag", "Punten", "Feedback"};
+
+        PdfPTable table = buildNewTable(QUESTIONS_COLUMN_WIDTHS);
+        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+
+        for (String columnName : columnNames) {
+            cell.setPhrase(new Phrase(columnName, font));
+            table.addCell(cell);
+        }
 
         return table;
     }
 
     private PdfPTable getExamQuestionsTable() {
-        String[] columnNames = {"Vraag", "Punten", "Feedback"};
-
         PdfPTable table = buildNewTable(QUESTIONS_COLUMN_WIDTHS);
-
-        for (String columnName : columnNames) {
-            table.addCell(columnName);
-        }
+        Font font = FontFactory.getFont(FontFactory.HELVETICA);
 
         for (StudentExamAnswer studentExamAnswer : studentExam.getStudentExamAnswers()) {
-            table.addCell(String.format("Vraag %d", studentExamAnswer.getQuestionNumber()));
-            table.addCell(String.format("%d / %d", studentExamAnswer.getPointsAttained(),
-                    studentExamAnswer.getAttainablePoints()));
-            table.addCell(studentExamAnswer.getFeedback());
+            cell.setPhrase(new Phrase(String.format("Vraag %d", studentExamAnswer.getQuestionNumber()), font));
+            table.addCell(cell);
+            cell.setPhrase(new Phrase(String.format("%d / %d", studentExamAnswer.getPointsAttained(),
+                    studentExamAnswer.getAttainablePoints()), font));
+            table.addCell(cell);
+            cell.setPhrase(new Phrase(studentExamAnswer.getFeedback(), font));
+            table.addCell(cell);
         }
 
         return table;
@@ -105,10 +124,14 @@ public class PDFExporter {
         String[] rowValues = getExamInfoTableValues();
 
         PdfPTable table = buildNewTable(EXAM_INFO_COLUMN_WIDTHS);
+        Font font = FontFactory.getFont(FontFactory.HELVETICA);
 
         for (int row = 0; row < rowNames.length; row++) {
-            table.addCell(rowNames[row]);
-            table.addCell(rowValues[row]);
+            cell.setPhrase(new Phrase(rowNames[row], font));
+            table.addCell(cell);
+
+            cell.setPhrase(new Phrase(rowValues[row], font));
+            table.addCell(cell);
         }
 
         return table;
